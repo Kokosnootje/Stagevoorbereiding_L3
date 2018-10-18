3caaba08c18956541c709140dd4763ee16538ba6
okapifan <wxzegers@gmail.com>
2018-10-18 11:36:10 +0200

    Add Roles Seeder Add Permission Seeder Add User Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'password' => Hash::make('123456789'),
+        ]);
+    }
+}
