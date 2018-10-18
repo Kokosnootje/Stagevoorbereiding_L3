b786c067dc4c3a456cc450c15caa4cb8a9b107f3
ALostTeddyBear <AtlasFlame_@hotmail.com>
2018-10-18 14:16:27 +0200

    Added User model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,11 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::all();
 
-
-        return View::make('user.index')
-            ->with('user', $user);
     }
 
     /**
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-    <table class="table table-striped table-bordered">
-        <thead>
-        <tr>
-            <td>ID</td>
-            <td>Name</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($user as $users=> $value)
-            <tr>
-                <td>{{ $value->id }}</td>
-                <td>{{ $value->name }}</td>
-
-                <td>
-                    <a class="btn btn-small btn-success" href="{{ URL::to('$users/' . $value->id) }}">Show this user</a>
-                    <a class="btn btn-small btn-info" href="{{ URL::to('users/' . $value->id . '/edit') }}">Edit this user</a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
-</div>
-</body>
-</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Atlas
+ * Date: 18/10/2018
+ * Time: 12:34
+ */
