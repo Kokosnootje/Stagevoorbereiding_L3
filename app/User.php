52f7f7f01bee7049628e56fce8e27babec68acfa
ALostTeddyBear <AtlasFlame_@hotmail.com>
2018-10-18 13:16:43 +0200

    Added User model

--- app/User.php
+++ app/User.php
@@ -9,7 +9,8 @@ class User extends Authenticatable
 {
     use Notifiable;
     /**
-     * The attributes that are mass assignabl.
+     * The attributes that are mass assignable.
+     *
      *
      * @var array
      */
