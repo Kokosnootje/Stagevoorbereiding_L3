27c2b37b3f4660d7f3e036c747b42bb96947d1e2
Kokosnootje <judacat@hotmail.com>
2018-10-31 09:25:40 +0100

    Removed Unneeded Auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
 
 Route::resource('user', 'UserController');
 
-Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
