4d6a184f82e3780c4361e80252395964ca5e4e3f
ALostTeddyBear <AtlasFlame_@hotmail.com>
2018-11-19 14:13:47 +0100

    Log uit is vervangen door Uitloggen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                             <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();">Log uit</a>
+                                   document.getElementById('logout-form').submit();">Uitloggen</a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
