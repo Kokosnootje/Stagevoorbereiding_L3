b85ce3857e715b89aea57b9e4593e1b1f17db966
Kokosnootje <judacat@hotmail.com>
2018-11-22 20:43:08 +0100

    "Fixed" logout in url giving a method not allowed error

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ Route::group(['middleware' => ['auth']], function () {
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
 $this->post('login', 'Auth\LoginController@login');
 $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+$this->get('logout', 'Auth\LoginController@logout')->name('logout');
