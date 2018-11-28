7c1a76da92d3f3b5bf7188a86ea63f60b489540d
Kokosnootje <judacat@hotmail.com>
2018-11-28 14:02:47 +0100

    Standaard is nu 0 bij groot scherm

--- resources/views/points/score.blade.php
+++ resources/views/points/score.blade.php
@@ -4,6 +4,8 @@
         <br />
         @if(!empty($house->current_points()->score))
             {{$house->current_points()->score}}
+            @else
+            0
         @endif
         <br />
         <img src="{{asset('photo/logo'.$house->id.'.png')}}" id="img{{$house->id}}">
